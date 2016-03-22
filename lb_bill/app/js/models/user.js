fed0ef806a401a6f1026c0f97b9772d3fb3dcf48
lukegeiken4 <luke.geiken@gmail.com>
2016-03-21 22:26:38 -0500

    working bill frontend app

--- lb_bill/app/js/models/user.js
+++ lb_bill/app/js/models/user.js
@@ -10,19 +10,19 @@ define(["backbone"],
 	            confirmPassword: ""
 	        },
 	        validate: function( object ){
-	        	// if(firstName==null || lastName==null || email==null || password==null || confirmPassword==null ) {
-	        	// 	return {error: "You have an invalid first and last name"};
-	        	// }
-	         //    if(attributes.firstName == "" || attributes.lastName == "" ){
-	         //        return {error: "You have an invalid first and last name"};
-	         //    }
-	         //    if(attributes.password == attributes.confirmPassword ){
-	         //        return {error: "Your passwords do not match"};
-	         //    }
-	         //    if( /(.+)@(.+){2,}\.(.+){2,}/.test(email) ){
-	         //        return {error: "Your email is invalid"};
-	         //    }
-	         //     return {status: "New user is valid"};
+	        	if(firstName==null || lastName==null || email==null || password==null || confirmPassword==null ) {
+	        		return {error: "You have an invalid first and last name"};
+	        	}
+	            if(firstName == "" || lastName == "" ){
+	                return {error: "You have an invalid first and last name"};
+	            }
+	            if(password == confirmPassword ){
+	                return {error: "Your passwords do not match"};
+	            }
+	            if( /(.+)@(.+){2,}\.(.+){2,}/.test(email) ){
+	                return {error: "Your email is invalid"};
+	            }
+	             return {status: "New user is valid"};
 	        },
 	        initialize: function(){
 	            console.log("New User Has Asked To Be Created");
